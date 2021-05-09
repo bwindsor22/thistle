bb8bd55e12b3b720f01ae78dfe29760c8d3729ff
Brad Windsor <bwindsor22@gmail.com>
2021-05-08 23:37:27 -0400

    add lsh working run in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use thistle::prelude::LshMem;
+
 fn main() {
     println!("run");
+    // 2 rows w/ dimension 3.
+    let p = &[vec![1., 1.5, 2.],
+    vec![2., 1.1, -0.3]];
+
+    // Do one time expensive preprocessing.
+    let n_projections = 9;
+    let n_hash_tables = 30;
+    let dim = 10;
+    let dim = 3;
+    let mut lsh = LshMem::new(n_projections, n_hash_tables, dim)
+    .srp()
+    .unwrap();
+    lsh.store_vecs(p);
+
+    // Query in sublinear time.
+    let query = &[1.1, 1.2, 1.2];
+    let out = lsh.query_bucket(query).unwrap();
+
+    for vecs in out {
+        println!("{:?}", vecs);
+    }
 }
